48eafca215c322ab129f808582e76754569b1c80
Carter Stanton <carter.stanton@humanit.govcloud>
2022-05-24 16:14:18 -0400

    Removal of unnecessary code.

--- components/charts/CoinPriceChartConfig.js
+++ components/charts/CoinPriceChartConfig.js
@@ -1,22 +1,4 @@
 export const Config = {
-  // plugins: {
-  //   legend: {
-  //     display: true,
-  //   },
-  // },
-  // lineHeightAnnotation: {
-  //   always: true,
-  //   lineWeight: 1.5,
-  // },
-  // animation: {
-  //   duration: 1,
-  // },
-  // maintainAspectRatio: false,
-  // responsive: true,
-  // scales: {
-  //   x: { display: true },
-  //   y: { display: true },
-  // },
   plugins: {
     legend: {
       display: false,
