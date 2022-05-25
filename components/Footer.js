fb4f14fe8e52a3ec7cfe5031e8ec642ab7a3502f
Carter Stanton <carter.stanton@humanit.govcloud>
2022-05-25 14:32:41 -0400

    Added Landmark role to `<footer>` HTML tag.
    Following the MDN guide on accessibility, the Landmark Role of `footer` was added to the appropriate HTML tag.

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from './styles/Footer.module.css';
 
 export default function Footer() {
   return ( 
-    <footer className={styles.footer}>
+    <footer role='footer' className={styles.footer}>
       <Link href="https://carterstanton.dev" >
         <a target="_blank" rel="noopener noreferrer">
           carterstanton.dev
